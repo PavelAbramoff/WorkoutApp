0d01b1f667fffb860ddfc97a415ea03567a37d80
Pavel.818 <Pavel.818@yandex.ru>
2023-10-26 22:57:36 -0700

    add-some-elements

--- WorkoutApp/ViewController.swift
+++ WorkoutApp/ViewController.swift
@@ -8,13 +8,103 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let userPhotoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = #colorLiteral(red: 0.7607843137, green: 0.7607843137, blue: 0.7607843137, alpha: 1)
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.layer.borderWidth = 5
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let userNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Your name"
+        label.textColor = .specialGray
+        label.font = .robotoMedium24()
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.5
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var addWorkoutButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .specialYellow
+        button.layer.cornerRadius = 10
+        button.setTitle("Add workout", for: .normal)
+        button.setImage(UIImage(named: "plus"), for: .normal)
+        button.tintColor = .specialDarkGreen
+        button.titleLabel?.font = .robotoMedium12()
+        button.imageEdgeInsets = .init(top: 0, left: 20, bottom: 15, right: 0)
+        button.titleEdgeInsets = .init(top: 50, left: -40, bottom: 0, right: 0)
+        button.addTarget(self, action: #selector(addWorkoutButtonTapped), for: .touchUpInside)
+        button.addShadowOnView()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let calendarView = CalendarView()
+    private let weatherView = WeatherView()
+    
+    override func viewDidLayoutSubviews() {
+        userPhotoImageView.layer.cornerRadius = userPhotoImageView.frame.width / 2
+    }
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
-        // Do any additional setup after loading the view.
+        
+        setupViews()
+        setConstraints()
     }
-
-
+    
+    private func setupViews() {
+        view.backgroundColor = .specialBackground
+        
+        view.addSubview(calendarView)
+        view.addSubview(userPhotoImageView)
+        view.addSubview(userNameLabel)
+        view.addSubview(addWorkoutButton)
+        view.addSubview(weatherView)
+    }
+    
+    @objc private func addWorkoutButtonTapped() {
+        print("TAP")
+    }
 }
+
+//MARK: - Set Constraints
+
+extension ViewController {
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            userPhotoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            userPhotoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            userPhotoImageView.heightAnchor.constraint(equalToConstant: 100),
+            userPhotoImageView.widthAnchor.constraint(equalToConstant: 100),
+            
+            calendarView.topAnchor.constraint(equalTo: userPhotoImageView.centerYAnchor),
+            calendarView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            calendarView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            calendarView.heightAnchor.constraint(equalToConstant: 70),
+            
+            userNameLabel.bottomAnchor.constraint(equalTo: calendarView.topAnchor, constant: -10),
+            userNameLabel.leadingAnchor.constraint(equalTo: userPhotoImageView.trailingAnchor, constant: 5),
+            userNameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            
+            addWorkoutButton.topAnchor.constraint(equalTo: calendarView.bottomAnchor, constant: 5),
+            addWorkoutButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            addWorkoutButton.heightAnchor.constraint(equalToConstant: 80),
+            addWorkoutButton.widthAnchor.constraint(equalToConstant: 80),
+            
+            weatherView.topAnchor.constraint(equalTo: calendarView.bottomAnchor, constant: 5),
+            weatherView.leadingAnchor.constraint(equalTo: addWorkoutButton.trailingAnchor, constant: 10),
+            weatherView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            weatherView.heightAnchor.constraint(equalToConstant: 80)
+        ])
+    }
+}
+
 
---
+++ WorkoutApp/WeatherView.swift
@@ -0,0 +1,103 @@
+//
+//  WeatherView.swift
+//  WorkoutApp
+//
+//  Created by Pavel Абрамов on 2023-10-25.
+//
+
+import UIKit
+
+class WeatherView: UIView {
+    
+    private let weatherIconImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.image = UIImage(named: "sun")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let weatherStatusLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Loading..."
+        label.textColor = .gray
+        label.font = .robotoMedium18()
+        label.textColor = .specialGray
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.6
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let weatherDiscriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Хорошая погода, чтобы позаниматься на улице"
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.8
+        label.numberOfLines = 2
+        label.font = .robotoMedium14()
+        label.textColor = .specialGray
+        label.font = .systemFont(ofSize: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        setContraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        backgroundColor = .white
+        layer.cornerRadius = 10
+        translatesAutoresizingMaskIntoConstraints = false
+        addShadowOnView()
+        
+        addSubview(weatherIconImageView)
+        addSubview(weatherStatusLabel)
+        addSubview(weatherDiscriptionLabel)
+    }
+}
+
+//MARK: - Set Contraints
+
+extension WeatherView {
+
+    private func setContraints() {
+
+        NSLayoutConstraint.activate([
+            //weatherIconImageView
+            weatherIconImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            weatherIconImageView.trailingAnchor.constraint(equalTo: trailingAnchor,
+                                                           constant: -10),
+            weatherIconImageView.widthAnchor.constraint(equalToConstant: 60),
+            weatherIconImageView.heightAnchor.constraint(equalToConstant: 60),
+            
+            //weatherStatusLabel
+            weatherStatusLabel.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            weatherStatusLabel.leadingAnchor.constraint(equalTo: leadingAnchor,
+                                                        constant: 10),
+            weatherStatusLabel.trailingAnchor.constraint(equalTo: weatherIconImageView.leadingAnchor,
+                                                         constant: -10),
+            weatherStatusLabel.heightAnchor.constraint(equalToConstant: 20),
+
+            //weatherDiscriptionLabel
+            weatherDiscriptionLabel.topAnchor.constraint(equalTo: weatherStatusLabel.bottomAnchor,
+                                                         constant: 0),
+            weatherDiscriptionLabel.leadingAnchor.constraint(equalTo: leadingAnchor,
+                                                             constant: 10),
+            weatherDiscriptionLabel.trailingAnchor.constraint(equalTo: weatherIconImageView.leadingAnchor,
+                                                              constant: -10),
+            weatherDiscriptionLabel.bottomAnchor.constraint(equalTo: bottomAnchor,
+                                                            constant: -5)
+        ])
+    }
+}
+
+
+
