52c00c0033714112714f7e911787e6f9098bc349
Pavel.818 <Pavel.818@yandex.ru>
2023-11-05 22:35:13 -0800

    create_statisticView_and_tabBar

---
+++ WorkoutApp/StatisticViewController.swift
@@ -0,0 +1,82 @@
+//
+//  StatisticViewController.swift
+//  WorkoutApp
+//
+//  Created by Pavel Абрамов on 2023-11-05.
+//
+
+import UIKit
+
+class StatisticViewController: UIViewController {
+    
+    private let statisticLabel: UILabel = {
+        let label = UILabel()
+        label.text = "STATISTIC"
+        label.font = .robotoMedium24()
+        label.textColor = .specialGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var segmentedControl: UISegmentedControl = {
+        let segmentedControl = UISegmentedControl(items: ["Week", "Month"])
+        segmentedControl.selectedSegmentIndex = 1
+        segmentedControl.backgroundColor = .specialGreen
+        segmentedControl.selectedSegmentTintColor = .specialYellow
+        let font = UIFont.robotoMedium16()
+        segmentedControl.setTitleTextAttributes([.font: font as Any,
+                                                 .foregroundColor: UIColor.white], for: .normal)
+        segmentedControl.setTitleTextAttributes([.font : font as Any,
+                                                 .foregroundColor: UIColor.specialGray], for: .selected)
+        segmentedControl.addTarget(self, action: #selector(segmentedChange), for: .valueChanged)
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        return segmentedControl
+    }()
+    
+    private let exercisesLabel = UILabel(text: "Exercises")
+    private let tableView = StatisticTableView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+        setConstraints()
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = .specialBackground
+        
+        view.addSubview(statisticLabel)
+        view.addSubview(segmentedControl)
+        view.addSubview(exercisesLabel)
+        view.addSubview(tableView)
+        
+    }
+    
+    @objc private func segmentedChange() {
+        print(segmentedControl.selectedSegmentIndex)
+    }
+}
+
+extension StatisticViewController {
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            statisticLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            statisticLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            segmentedControl.topAnchor.constraint(equalTo: statisticLabel.bottomAnchor, constant: 20),
+            segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            exercisesLabel.topAnchor.constraint(equalTo: segmentedControl.bottomAnchor, constant: 10),
+            exercisesLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            exercisesLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            tableView.topAnchor.constraint(equalTo: exercisesLabel.bottomAnchor, constant:0),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
+    }
+}
