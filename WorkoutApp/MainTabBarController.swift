9315cc4c6930f8b6d7b6490ddcf241b3a03ef267
Pavel.818 <Pavel.818@yandex.ru>
2024-01-16 22:24:51 -0800

    add_setting_module

--- WorkoutApp/MainTabBarController.swift
+++ WorkoutApp/MainTabBarController.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 
-
 class MainTabBarController: UITabBarController {
     
     override func viewDidLoad() {
@@ -21,23 +20,26 @@ class MainTabBarController: UITabBarController {
         tabBar.backgroundColor = .specialTabBar
         tabBar.tintColor = .specialDarkGreen
         tabBar.unselectedItemTintColor = .specialGray
-        tabBar.layer.borderWidth = 2
+        tabBar.layer.borderWidth = 1
         tabBar.layer.borderColor = UIColor.specialLightBrown.cgColor
     }
     
     private func setupItems() {
         let mainVC = MainViewController()
         let statisticVC = StatisticViewController()
+        let profileVC = ProfileViewController()
         
-        setViewControllers([mainVC, statisticVC], animated: true)
+        setViewControllers([mainVC, statisticVC, profileVC], animated: true)
         
         guard let items = tabBar.items else { return }
         
         items[0].title = "Main"
         items[1].title = "Statistic"
+        items[2].title = "Profile"
         
         items[0].image = UIImage(named: "mainTabBar")
         items[1].image = UIImage(named: "statisticTabBar")
+        items[2].image = UIImage(named: "profileTabBar")
         
         UITabBarItem.appearance().setTitleTextAttributes([.font : UIFont(name: "Roboto-Bold", size: 12) as Any], for: .normal)
     }
--- WorkoutApp/RealmManager.swift
+++ WorkoutApp/RealmManager.swift
@@ -50,6 +50,30 @@ class RealmManager {
             model.workoutStatus = true
         }
     }
+    //USER
+    
+    func getResultUserModel() -> Results<UserModel> {
+        realm.objects(UserModel.self)
+    }
+    
+    func saveUserModel(_ model: UserModel) {
+        try! realm.write {
+            realm.add(model)
+        }
+    }
+    
+    func updateUserModel(model: UserModel) {
+        
+        let users = getResultUserModel()
+        
+        try! realm.write {
+            users[0].userFirstName = model.userFirstName
+            users[0].userSecondName = model.userSecondName
+            users[0].userHeight = model.userHeight
+            users[0].userWeight = model.userWeight
+            users[0].userTarget = model.userTarget
+            users[0].userImage = model.userImage
+        }
+    }
 }
-
 
